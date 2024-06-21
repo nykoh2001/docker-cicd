efb96a006d7570a826634f9071f0181bbd73f02b
nykoh2001 <nykoh2001@gmail.com>
2024-06-22 01:06:15 +0900

    [CHORE] initial commit

---
+++ src/test/java/org/sopt/dockercicd/DockerCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.dockercicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
